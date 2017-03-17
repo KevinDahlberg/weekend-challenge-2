6c847d15dcd3568af89de52ae27d979dd08a1f04
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-17 17:00:06 -0500

    added click functionality to buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,32 @@
 $(function(){
   console.log("Client.js is LIVE!");
+  //Global Variables
+
+  //inputs
+
+  //Buttons
+  //add
+  $("#addButton").on('click', function(){
+    console.log("Add button clicked");
+  });
+  //Subtract
+  $("#subtractButton").on('click', function(){
+    console.log("Subtract button clicked");
+  });
+  //Divide
+  $("#divideButton").on('click', function(){
+    console.log("Divide button clicked");
+  });
+  //Multiply
+  $("#multiplyButton").on('click', function(){
+    console.log("Multiply button clicked");
+  });
+  //Equals
+  $("#equalsButton").on('click', function(){
+    console.log("Equals button clicked");
+  });
+
+  //Functions
+
+  //called functions
 });
