ab29c77586f04589074b296f3d4215ceb96c29b1
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-17 18:02:57 -0500

    working on getting object put together to send to server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,36 +1,51 @@
 $(function(){
   console.log("Client.js is LIVE!");
   //Global Variables
-  var numberOne = $("#numberOne").val();
-  var numberTwo = $("#numberTwo").val();
-  var buttonPressed = null;
-  var sendCalc = {
-    firstNum: numberOne,
-    secondNum: numberTwo,
-    button: buttonPressed
-  };
+  var buttonPressed;
+  var sendCalc
   //inputs
 
   //Buttons
   //add
   $("#addButton").on('click', function(){
     console.log("Add button clicked");
+    buttonPushed($(this));
+    return buttonPressed;
+
   });
   //Subtract
   $("#subtractButton").on('click', function(){
     console.log("Subtract button clicked");
+    buttonPushed($(this));
+    return buttonPressed;
+
   });
   //Divide
   $("#divideButton").on('click', function(){
     console.log("Divide button clicked");
+    buttonPushed($(this));
+    return buttonPressed;
+
   });
   //Multiply
   $("#multiplyButton").on('click', function(){
     console.log("Multiply button clicked");
+    buttonPushed($(this));
+    return buttonPressed;
+
   });
   //Equals
   $("#equalsButton").on('click', function(){
     console.log("Equals button clicked");
+    var numberOne = $("#numberOne").val();
+    var numberTwo = $("#numberTwo").val();
+    // you are using a GET statement - maybe you dont need an object???
+    var sendCalc = {
+      firstNum: numberOne,
+      secondNum: numberTwo,
+      button: buttonPressed
+    };
+    console.log(sendCalc);
     $.ajax({
       type: 'GET',
       url: 'calculate',
@@ -42,6 +57,11 @@ $(function(){
   });
 
   //Functions
-
+  function buttonPushed(button){
+    buttonPressed = button;
+    //console.log(buttonPressed);
+    return buttonPressed;
+  }
   //called functions
+  console.log(buttonPressed);
 });
