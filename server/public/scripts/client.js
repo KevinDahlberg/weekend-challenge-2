61c7af4f2b907c6a06267c68ed7e5c4815029f38
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-19 04:25:09 -0500

    buttons on DOM and numbers displaying in input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -57,6 +57,73 @@ $(function(){
     });
   });
 
+  //clear button
+  $("#clearButton").on('click', function(){
+    console.log("Clear button clicked");
+    $("#numberOne").val("");
+    $("#numberTwo").val("");
+  });
+
+  //zero button
+  $("#zeroButton").on('click', function(){
+    $("#numberInput").append("<span>0</span>");
+    console.log("Zero button clicked");
+  });
+
+  //one button
+  $("#oneButton").on('click', function(){
+    $("#numberInput").append("<span>1</span>");
+    console.log("One button clicked");
+  });
+
+  //two button
+  $("#twoButton").on('click', function(){
+    $("#numberInput").append("<span>2</span>");
+    console.log("Two button clicked");
+  });
+
+  //three button
+  $("#threeButton").on('click', function(){
+    $("#numberInput").append("<span>3</span>");
+    console.log("Three button clicked");
+  });
+
+  //four button
+  $("#fourButton").on('click', function(){
+    $("#numberInput").append("<span>4</span>");
+    console.log("Four button clicked");
+  });
+
+  //five button
+  $("#fiveButton").on('click', function(){
+    $("#numberInput").append("<span>5</span>");
+    console.log("Five button clicked");
+  });
+
+  //six button
+  $("#sixButton").on('click', function(){
+    $("#numberInput").append("<span>6</span>");
+    console.log("Six button clicked");
+  });
+
+  //seven button
+  $("#sevenButton").on('click', function(){
+    $("#numberInput").append("<span>7</span>");
+    console.log("Seven button clicked");
+  });
+
+  //eight button
+  $("#eightButton").on('click', function(){
+    $("#numberInput").append("<span>8</span>");
+    console.log("Eight button clicked");
+  });
+
+  //nine button
+  $("#nineButton").on('click', function(){
+    $("#numberInput").append("<span>9</span>");
+    console.log("Nine button clicked");
+  });
+
   //Functions
   function displayResult(number){
     $("#container").append("<p>"+number+"</p>");
